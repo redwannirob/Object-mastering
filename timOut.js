5be97778bc1e82b1561dbc45c93666dcbf5f8506
Redwan Nirob <Redwan Nirob>
2021-06-24 19:05:47 +0600

    learned about set timeout and set interval

---
+++ timOut.js
@@ -0,0 +1,16 @@
+function doSomething(task){
+    console.log(3333)
+}
+console.log(2222);
+// setTimeout(() => {
+//     console.log('lazy and waiting')
+// }, 4000);
+setInterval(() => {
+    console.log('Doing it');
+}, 3000);
+console.log(4444);
+console.log(5555);
+console.log(6666);
+console.log(7777);
+console.log(8888);
+console.log(9999);
